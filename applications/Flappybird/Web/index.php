7539f2a10e6061f483f9fa8e5ba0756cd04c91bb
liangguifeng <1476982312@qq.com>
2018-09-19 22:40:48 +0800

    飞翔的小鸟小游戏

---
+++ applications/Flappybird/Web/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+<title>飞翔的小鸟-多人在线版</title>
+<meta name="description" content="">
+<meta name="keywords" content="flappy bird 多人在线版 workerman">
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+<script src="/js/jquery-2.1.0.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+</head>
+<body ondragstart='return false' onselectstart ='return false'>
+ <div class="container">
+    <div class="row clearfix">
+    <div class="col-md-1 column">
+        </div>
+        <div class="col-md-11 column">
+        <input type="hidden" id="nickname" >
+        <canvas id="canvas" width="800" height="512" style="margin: 3px auto 0px auto;"></canvas>
+        <script type="text/javascript" src="/js/main.js"></script>
+        <script type="text/javascript">
+/*760*90，创建于2014-6-4*/
+var cpro_id = "u1576789";
+</script>
+<!-- <script src="http://cpro.baidustatic.com/cpro/ui/c.js" type="text/javascript"></script> -->
+
+        </div>
+    </div>
+</div>
+<!-- <script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fabcf9330bef79b4aba5b24fa373506d9' type='text/javascript'%3E%3C/script%3E"));
+</script> -->
+<br>
+<center style="color:#333333;font-weight:bold;">版权所有：西京学院易班学生工作站</center>
+<center style="color:#333333;font-weight:bold;"><a href="https://fanerblog.xin" target="_blank">开发者：犯二青年</a></center>
+</body>
+</html>
